8916d7257775c50d247dce8de050cd647e54aaff
Evgeniy Kiru <kiru_e@mail.ru>
2021-07-14 11:56:14 +0300

    CourseService вместо CourseLister

--- src/main/java/ru/mtsteta/courses/controller/CourseController.java
+++ src/main/java/ru/mtsteta/courses/controller/CourseController.java
@@ -1,26 +1,35 @@
 package ru.mtsteta.courses.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.mtsteta.courses.domain.Course;
-import ru.mtsteta.courses.service.CourseLister;
+import ru.mtsteta.courses.service.CourseService;
 import ru.mtsteta.courses.service.StatisticsCounter;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/course")
 @RequiredArgsConstructor
 public class CourseController {
-    private final CourseLister courseLister;
+    private final CourseService courseService;
     private final StatisticsCounter statisticsCounter;
 
     @GetMapping("/interesting")
+    @ResponseBody
     public List<Course> getInterestingCourses() {
         statisticsCounter.countHandlerCall();
         // У нас есть бизнес инсайт, что все интересные курсы написал Вася
-        return courseLister.coursesByAuthor("Вася");
+        return courseService.coursesByAuthor("Вася");
+    }
+
+    @GetMapping
+    public String courseTable(Model model) {
+        model.addAttribute("courses", courseService.findAll());
+        return "course_list";
     }
 }
---
+++ src/main/java/ru/mtsteta/courses/service/CourseService.java
@@ -0,0 +1,24 @@
+package ru.mtsteta.courses.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.mtsteta.courses.dao.CourseRepository;
+import ru.mtsteta.courses.domain.Course;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public List<Course> coursesByAuthor(String name) {
+        List<Course> allCourses = courseRepository.findAll();
+        return allCourses.stream().filter(course -> course.getAuthor().equals(name)).collect(Collectors.toList());
+    }
+
+    public List<Course> findAll() {
+        return courseRepository.findAll();
+    }
+}
