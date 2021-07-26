0929bfd288f4162fb21afd73e92ac6f65651219a
Evgeniy Kiru <kiru_e@mail.ru>
2021-07-26 18:31:27 +0300

    Добавлен BCryptPasswordEncoder

---
+++ src/main/java/ru/mtsteta/courses/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package ru.mtsteta.courses.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    public void authConfigure(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder.encode("123"))
+                .roles("STUDENT")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN");
+    }
+}
